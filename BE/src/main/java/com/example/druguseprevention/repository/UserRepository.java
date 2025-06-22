3bca6dda48d70353d44d62bc808398bbe18fffff
HoangNDxFPT <hoangnguyenduc674@gmail.com>
2025-06-22 20:30:24 +0700

    lam phan quyen xem edit profile-self và admin edit

--- BE/src/main/java/com/example/druguseprevention/repository/UserRepository.java
+++ BE/src/main/java/com/example/druguseprevention/repository/UserRepository.java
@@ -15,4 +15,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     // ✅ Thêm dòng này để tìm tất cả user có role = MEMBER
     List<User> findByRoleAndDeletedFalse(Role role);
+    boolean existsByEmail(String email);
 }
